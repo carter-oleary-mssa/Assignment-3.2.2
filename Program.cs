e88f9338c50e9ac5f00fef36751ad84df8da3c56
carter-oleary <carter.m.oleary@outlook.com>
2025-04-22 14:05:28 -0400

    Formatting

--- Program.cs
+++ Program.cs
@@ -43,7 +43,7 @@ void PrintMatrix(int[,] arr)
 {
     for(int i = 0;i < arr.GetLength(0); i++)
     {
-        for(int j = 0; j < arr.GetLength(1); j++) { Console.Write($"{arr[i, j]} "); }
-        Console.WriteLine();
+        for(int j = 0; j < arr.GetLength(1); j++) { Console.Write($"| {arr[i, j], 2} "); }
+        Console.WriteLine("|");
     }
 }
